8121bcc5ebfb2a5ecaa0052c52e88ee4b15444e9
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-11-18 14:47:24 -0600

    cambio_foto
    faltante de implementar con el diseño existente

--- Galeria - Compose/app/src/main/java/edu/uvg/com/example/galeriacompose/MainActivity.kt
+++ Galeria - Compose/app/src/main/java/edu/uvg/com/example/galeriacompose/MainActivity.kt
@@ -1,8 +1,12 @@
 package edu.uvg.com.example.galeriacompose
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.foundation.Image
@@ -24,11 +28,19 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,6 +49,7 @@ import androidx.compose.ui.unit.dp
 import edu.uvg.com.example.galeriacompose.ui.theme.ColoresApp
 import edu.uvg.com.example.galeriacompose.ui.theme.FormasApp
 import edu.uvg.com.example.galeriacompose.ui.theme.FuentesApp
+
 
 
 class MainActivity : ComponentActivity() {
@@ -51,26 +64,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun miPerfil_pantalla() {
-
-    var nombreUsuario = "Josué Say"
-    var correoUsuario = "say22801@correo.com"
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+    val pickImageLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = ColoresApp.colorFondo)
+            .background(color = ColoresApp.colorFondo),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
+        imageUri?.let {
+            val imageBitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(it))
+            Image(
+                bitmap = imageBitmap.asImageBitmap(),
+                contentDescription = "Selected Image",
+                modifier = Modifier.size(150.dp).clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        } ?: Image(
+            painter = painterResource(id = R.drawable.icon_perfil2),
+            contentDescription = "Default Image",
+            modifier = Modifier.size(150.dp).clip(CircleShape)
+        )
 
-        tituloPerfil()
-        fotoPerfil()
-        cambiarImagen()
-        //Spacer(modifier = Modifier.height(90.dp)) // Espacio antes de la nueva sección
-        //Spacer(modifier = Modifier.weight(1f))  // Espaciador que ocupa todo el espacio restante
-        infoUsuario(nombreUsuario = nombreUsuario, correoUsuario = correoUsuario)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { pickImageLauncher.launch("image/*") }) {
+            Text("Editar Imagen")
+        }
     }
-
-
 }
+
+
 
 // TITULO APP
 @Composable
@@ -218,7 +249,6 @@ fun infoUsuario(nombreUsuario: String, correoUsuario: String) {
         }
     }
 }
-
 
 @Preview
 @Composable
